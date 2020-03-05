d34ed8176c201d5185ac5f6fdc292210fd7a2623
Masaki Furuta <masaki.furuta@gmail.com>
2020-03-05 12:25:56 +0900

    Add Google Calendar

---
+++ src/gcalendar/background.js
@@ -0,0 +1,23 @@
+const configWindow = {
+  bounds: {
+    width: 768,
+    height: 1024
+  },
+  minWidth: 768,
+  minHeight: 1024,
+  resizable: true,
+  frame: "none",
+  alwaysOnTop: true,
+  hidden: true
+};
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create("index.html", configWindow, function(appWin) {
+    appWin.contentWindow.addEventListener("DOMContentLoaded", function(e) {
+      var webview = appWin.contentWindow.document.querySelector("webview");
+      webview.src = "https://calendar.google.com/calendar/b/1/r/customday";
+
+      appWin.show();
+    });
+  });
+});
